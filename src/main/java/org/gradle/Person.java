519dace1657a842964add8c20fde30e9dddde549
Andres Felipe Torres L <andres.torres@ceiba.com.co>
2018-07-25 11:01:39 -0500

    Prueba Cobertura

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -5,6 +5,7 @@ import org.apache.commons.collections.list.GrowthList;
 public class Person {
     private final String name;
     public String newValue;
+    public boolean otroNewValue;
 
     public Person(String name) {
         this.name = name;
